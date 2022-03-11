06ec878a61618273bdf99a120cc675a7e10d1e05
misiucodes <meeshtran15@gmail.com>
2022-03-11 16:11:14 -0500

    added 2nd hero section

--- src/components/pages/Homepage/Data.js
+++ src/components/pages/Homepage/Data.js
@@ -1,6 +1,6 @@
 export const homeObjOne = {
-  lightBg: true,
-  lightText: false,
+  lightBg: false,
+  lightText: true,
   lightTextDesc: true,
   topLine: '',
   headline: 'Give people a place of refuge in times of crisis.',
@@ -8,48 +8,21 @@ export const homeObjOne = {
     '',
   buttonLabel: 'Get Started',
   imgStart: '',
-  img: 'images/family.svg',
+  img: 'images/help.svg',
   alt: 'Mother, Father and two children looking at a globe.'
 };
 
 export const homeObjTwo = {
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
   topLine: '',
   headline: 'Help for people fleeing Ukraine',
   description:
     'Right now, refugees fleeing from the war in Ukraine are in need of urgent temporary housing. Your space can provide comfort to people making a live-saving journey and be a powerful way of welcoming those fleeing to help integrate within the community faster.',
   buttonLabel: 'Learn More',
-  imgStart: '',
-  img: '',
-  alt: ''
+  imgStart: 'start',
+  img: 'images/ukraine.svg',
+  alt: 'Ukrainian girl'
 };
 
-export const homeObjThree = {
-  lightBg: true,
-  lightText: false,
-  lightTextDesc: false,
-  topLine: 'EASY SETUP',
-  headline: 'Super fast and simple onboarding process',
-  description:
-    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
-  buttonLabel: 'Start Now',
-  imgStart: 'start',
-  img: 'images/svg-7.svg',
-  alt: 'Vault'
-};
-
-export const homeObjFour = {
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: 'DATA ANALYTICS',
-  headline: 'Every transaction is stored on our secure cloud database',
-  description:
-    'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
-  buttonLabel: 'Sign Up Now',
-  imgStart: 'start',
-  img: 'images/svg-8.svg',
-  alt: 'Vault'
-};
--- src/components/pages/Homepage/Home.js
+++ src/components/pages/Homepage/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import HeroSection from '../../HeroSection';
-import { homeObjOne } from './Data';
+import { homeObjOne, homeObjTwo } from './Data';
 
 
 function Home() {
   return (
     <>
       <HeroSection {...homeObjOne} />
+      <HeroSection {...homeObjTwo} />
     </>
   );
 }
